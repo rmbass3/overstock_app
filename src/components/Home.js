a7ef16637b3a1987418725b15fe8cf351c267fea
Ryan <rmbass3@gmail.com>
2021-10-23 03:39:18 -0400

    Added responsive home title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
+import {Button} from 'react-bootstrap';
 
 function Home(props) {
   return (
     <div className="home" id="home">
       <div className="container title-container">
-        <h2 className="title-intro">FIND THE PERFECT FLOORING</h2>
+        <h2 className="title-intro">FIND THE PERFECT</h2>
+        <h2 className="title-intro">FLOORING WITH</h2>
         <h1 className="title-overstock">OVERSTOCK</h1>
         <h3 className="title-end">DISCOUNT FLOORING</h3>
         <hr className="title-hr"/>
+        <Button size="lg" className="title-button btn-secondary">
+          Get a quote today
+        </Button>
       </div>
     </div>
   )
