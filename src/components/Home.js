47e5392da4786bc998e3d8a535a1f3ffdd7fa431
Ryan <rmbass3@gmail.com>
2021-10-23 00:12:51 -0400

    Fixed navbar, added home title

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,12 @@ import React from "react";
 function Home(props) {
   return (
     <div className="home" id="home">
+      <div className="container title-container">
+        <h2 className="title-intro">FIND THE PERFECT FLOORING</h2>
+        <h1 className="title-overstock">OVERSTOCK</h1>
+        <h3 className="title-end">DISCOUNT FLOORING</h3>
+        <hr className="title-hr"/>
+      </div>
     </div>
   )
 }
