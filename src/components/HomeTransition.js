ba3898530d1d7457218929a6a2974dd106c80cee
Ryan <rmbass3@gmail.com>
2021-10-22 22:06:28 -0400

    First commit

---
+++ src/components/HomeTransition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeTransition(props) {
+  return (
+    <div className="home-trans">
+
+    </div>
+  )
+}
+
+export default HomeTransition;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Navbar, Nav} from 'react-bootstrap';
+import logo from '../imgs/logo.png'
+function Header(props) {
+  return (
+    <div>
+      <Navbar expand="lg" className="header">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="" className="logo"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#gallery">Gallery</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header;
