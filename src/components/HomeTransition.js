b580d9cd85d2145d1aee8acf99b3de963e1d0efb
rmbass3 <rmbass3@gmail.com>
2021-10-25 19:32:55 -0400

    Added text and pictures for trans1

--- src/components/HomeTransition.js
+++ src/components/HomeTransition.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { ParallaxBanner } from 'react-scroll-parallax';
-import trans1img1 from '../imgs/trans1-img-right.jpg'
-import trans1img2 from '../imgs/trans1-img-left.jpg'
+import trans1img1 from '../imgs/trans1-img1.jpg'
+import trans1img2 from '../imgs/trans1-img2.jpg'
+import trans1img3 from '../imgs/trans1-img3.jpg'
+import trans1img4 from '../imgs/trans1-img4.jpg'
+import trans1img5 from '../imgs/trans1-img5.jpg'
+import trans1img6 from '../imgs/trans1-img6.jpg'
 import ScrollAnimation from "react-animate-on-scroll";
 
 function HomeTransition(props) {
@@ -78,6 +82,164 @@ function HomeTransition(props) {
                     </h4>
                     <p className="trans1-text">
                       Scratches from animal claws or active children running through the house have met their match. Unlike real wood floors, LVP is very durable with MIL wears from 12-22. Even with the heaviest traffic it’s scratch resistant.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </ScrollAnimation>
+          </div>
+        </div>
+        {/* Row Set 2 */}
+        <div className="row row-cols-1 row-cols-md-2 gx-0">
+          <div className="col order-1 order-md-1">
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce="true">
+              <div className="col trans1-text-col">
+                <div className="trans1-box-left p-3 p-md-0">
+                    <div className="container trans1-text-container ps-0 ps-md-3">
+                      <h4 className="trans1-title-text">
+                        Price
+                      </h4>
+                      <p className="trans1-text">
+                        Luxury vinyl plank flooring is a cost-effective way to update your home without breaking the bank. You can save thousands without sacrificing on style. Unlike real wood or even LVP at most stores, you can find quality LVP at Overstock Discount Flooring for less than half of typical “big box” stores.
+                      </p>
+                    </div>
+                    <div className="triangle-right-bottom d-none d-md-block"/>
+                </div>
+              </div>
+            </ScrollAnimation>
+          </div>
+          <div className="col order-2 order-md-2">
+            <ScrollAnimation animateIn="fadeInRight" animateOnce="true">
+              <div className="col trans1-img-col">
+                <ParallaxBanner
+                  className="parallax-trans1-right"
+                  layers={[
+                    {
+                      image: trans1img3,
+                      amount: 0.5
+                    }
+                  ]}
+                  style={{
+                    height: '314px'
+                  }}
+                >
+                <div className="triangle-left-top d-none d-md-block"/>
+                </ParallaxBanner> 
+              </div>
+            </ScrollAnimation>
+          </div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-2 gx-0">
+          <div className="col order-2 order-md-1">
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce="true">
+              <div className="col trans1-img-col">
+              <ParallaxBanner
+                  className="parallax-trans1-right"
+                  layers={[
+                    {
+                      image: trans1img4,
+                      amount: 0.5
+                    }
+                  ]}
+                  style={{
+                    height: '314px'
+                  }}
+                >
+                  <div className="triangle-right-top d-none d-md-block"/>
+                </ParallaxBanner> 
+              </div>
+            </ScrollAnimation>
+          </div>
+          <div className="col order-1 order-md-2">
+            <ScrollAnimation animateIn="fadeInRight" animateOnce="true">
+              <div className="col trans1-text-col">
+                <div className="trans1-box-right p-3 p-md-0">
+                  <div className="triangle-left-bottom d-none d-md-block"/>
+                  <div className="container trans1-text-container">
+                    <h4 className="trans1-title-text">
+                      Installation
+                    </h4>
+                    <p className="trans1-text">
+                      You won’t need to be a carpenter to install your new LVP floors. Some customers choose the “do-it-yourself” method or we can provide you with a recommend licensed installation team. 
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </ScrollAnimation>
+          </div>
+        </div>
+        {/* Row Set 3 */}
+        <div className="row row-cols-1 row-cols-md-2 gx-0">
+          <div className="col order-1 order-md-1">
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce="true">
+              <div className="col trans1-text-col">
+                <div className="trans1-box-left p-3 p-md-0">
+                  <div className="container trans1-text-container ps-0 ps-md-3">
+                    <h4 className="trans1-title-text">
+                      Look & Feel
+                    </h4>
+                    <p className="trans1-text">
+                      LVP is soft underfoot. As you walk, the flooring absorbs pressure. This flooring stays at room temperature to keep you comfortable, even in the winter.
+                    </p>
+                  </div>
+                  <div className="triangle-right-bottom d-none d-md-block"/>
+                </div>
+              </div>
+            </ScrollAnimation>
+          </div>
+          <div className="col order-2 order-md-2">
+            <ScrollAnimation animateIn="fadeInRight" animateOnce="true">
+              <div className="col trans1-img-col">
+                <ParallaxBanner
+                  className="parallax-trans1-right"
+                  layers={[
+                    {
+                      image: trans1img5,
+                      amount: 0.5
+                    }
+                  ]}
+                  style={{
+                    height: '314px'
+                  }}
+                >
+                <div className="triangle-left-top d-none d-md-block"/>
+                </ParallaxBanner> 
+              </div>
+            </ScrollAnimation>
+          </div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-2 gx-0">
+          <div className="col order-2 order-md-1">
+            <ScrollAnimation animateIn="fadeInLeft" animateOnce="true">
+              <div className="col trans1-img-col">
+              <ParallaxBanner
+                  className="parallax-trans1-right"
+                  layers={[
+                    {
+                      image: trans1img6,
+                      amount: 0.5
+                    }
+                  ]}
+                  style={{
+                    height: '314px'
+                  }}
+                >
+                  <div className="triangle-right-top d-none d-md-block"/>
+                </ParallaxBanner> 
+              </div>
+            </ScrollAnimation>
+          </div>
+          <div className="col order-1 order-md-2">
+            <ScrollAnimation animateIn="fadeInRight" animateOnce="true">
+              <div className="col trans1-text-col">
+                <div className="trans1-box-right p-3 p-md-0">
+                  <div className="triangle-left-bottom d-none d-md-block"/>
+                  <div className="container trans1-text-container">
+                    <h4 className="trans1-title-text">
+                      Low-Maintenance
+                    </h4>
+                    <p className="trans1-text">
+                      When it comes to maintenance, it doesn’t get any easier than LVP. These floors are 100% waterproof and are installed on a 5G click and lock system to ensure no water leakage. No wax? No polish? No problem. Today’s LVP doesn’t need added chemicals to hold its shine. Simply clean with a dust mop or for a deeper clean you can easily damp mop to keep sanitized and sterilized. Great for pet accidents or kids spills.
                     </p>
                   </div>
                 </div>
