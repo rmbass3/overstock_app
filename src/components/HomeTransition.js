773033d4eb8aa05a2c410cf40edfe3340858fa68
rmbass3 <rmbass3@gmail.com>
2021-10-25 03:58:01 -0400

    Added first row of home transition

--- src/components/HomeTransition.js
+++ src/components/HomeTransition.js
@@ -1,9 +1,30 @@
 import React from "react";
+import img1 from "../imgs/trans1-img-right.png"
 
 function HomeTransition(props) {
   return (
-    <div className="home-trans">
-
+    <div className="home-trans1">
+      <div className="trans1-container">
+        <div className="row row-cols-1 row-cols-md-2">
+          <div className="col trans1-col">
+            <div className="trans1-box-left">
+              <div className="container trans1-title-container">
+                <h3 className="trans1-title">
+                  Why Luxury Vinyl <br/>
+                  Plank (LVP) <br/>
+                  Flooring?
+                </h3>
+              </div>
+              <div className="triangle-left-bottom"/>
+            </div>
+          </div>
+          <div className="col">
+            <div className="trans1-img-right-container">
+              <div className="triangle-right-top"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
