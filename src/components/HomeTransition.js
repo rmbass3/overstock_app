288187db1f43616a394339fc53f78f0d583a2351
rmbass3 <rmbass3@gmail.com>
2021-10-25 12:29:36 -0400

    Added css for second transition row

--- src/components/HomeTransition.js
+++ src/components/HomeTransition.js
@@ -5,8 +5,8 @@ function HomeTransition(props) {
   return (
     <div className="home-trans1">
       <div className="trans1-container">
-        <div className="row row-cols-1 row-cols-md-2">
-          <div className="col trans1-col">
+        <div className="row row-cols-1 row-cols-md-2 gx-0">
+          <div className="col trans1-text-col">
             <div className="trans1-box-left">
               <div className="container trans1-title-container">
                 <h3 className="trans1-title">
@@ -15,12 +15,29 @@ function HomeTransition(props) {
                   Flooring?
                 </h3>
               </div>
-              <div className="triangle-left-bottom"/>
+              <div className="triangle-right-bottom"/>
             </div>
           </div>
-          <div className="col">
+          <div className="col trans1-img-col">
             <div className="trans1-img-right-container">
+              <div className="triangle-left-top"/>
+            </div>
+          </div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-2 gx-0">
+          <div className="col trans1-img-col">
+            <div className="trans1-img-left-container">
               <div className="triangle-right-top"/>
+            </div>
+          </div>
+          <div className="col trans1-text-col">
+            <div className="trans1-box-right">
+              <div className="triangle-left-bottom"/>
+              <div className="container trans1-text-container">
+                <p className="trans1-text">
+                  Scratches from animal claws or active children running through the house have met their match. Unlike real wood floors, LVP is very durable with MIL wears from 12-22, even with the heaviest traffic it’s scratch resistant.
+                </p>
+              </div>
             </div>
           </div>
         </div>
