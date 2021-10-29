9dc9c19730a8d5f1ba42c7cf467fa97c8aaafcb0
Ryan <rmbass3@gmail.com>
2021-10-28 23:55:49 -0400

    Finished about banned

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,9 @@
 import React from "react";
 import {Button} from 'react-bootstrap';
-import { fadeIn } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
 import { ParallaxBanner } from "react-scroll-parallax";
 import bgMain from '../imgs/bg-main-dark-2.jpg'
+import ScrollAnimation from "react-animate-on-scroll";
 
-const styles = {
-  fadeIn1: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(fadeIn, 'fadeIn')
-  },
-
-  fadeIn2: {
-    animation: 'x 2s',
-    animationName: Radium.keyframes(fadeIn, 'fadeIn')
-  },
-
-  fadeIn3: {
-    animation: 'x 3s',
-    animationName: Radium.keyframes(fadeIn, 'fadeIn')
-  }
-}
 
 function Home(props) {
   return (
@@ -41,8 +24,8 @@ function Home(props) {
           <div className="container home-title-container">
             <div className="text-center">
               <h2 className="title-intro">FIND THE PERFECT</h2>
-              <h2 className="title-intro" style={styles.fadeIn2}>FLOORING WITH</h2>
-              <div style={styles.fadeIn3}>
+              <h2 className="title-intro">FLOORING WITH</h2>
+              <div>
                 <h1 className="title-overstock">OVERSTOCK</h1>
                 <h3 className="title-end mt-2">DISCOUNT FLOORING</h3>
                 <Button 
