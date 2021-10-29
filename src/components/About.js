978f11db8a0f93d7fc2522dc6e09c02379f768f4
Ryan <rmbass3@gmail.com>
2021-10-28 23:23:54 -0400

    Added about banner

--- src/components/About.js
+++ src/components/About.js
@@ -4,20 +4,24 @@ import ScrollAnimation from "react-animate-on-scroll";
 import bgAbout from '../imgs/bg-main-dark.jpg';
 
 function About(props){
-    return (
-        <div id="about">
-            <ScrollAnimation
-            className="about-banner"
-            layers={[
-                {
-                  image: bgAbout,
-                  amount: 0.5,
-                },
-            ]}
-            >
-
-            </ScrollAnimation>
+  return (
+    <div id="about">
+      <ParallaxBanner
+        className="about-banner"
+        layers={[
+          {
+            image: bgAbout,
+            amount: 0.5,
+          },
+        ]}
+      >
+        <div className="about">
+          <div className="container title-container">
+            <h1 className="about-title">High Point's Most Trusted Flooring Company</h1>
+          </div>
         </div>
+      </ParallaxBanner>
+    </div>
     )
 }
 
