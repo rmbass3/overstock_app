9e520489f6d39d0adc675b96be84cef20223bb7a
Ryan <rmbass3@gmail.com>
2021-10-29 16:59:35 -0400

    Fixed first about row

--- src/components/About.js
+++ src/components/About.js
@@ -23,42 +23,37 @@ function About(props){
       </ParallaxBanner>
 
       <div className="about-container">
-        <div className="row row-cols-1 row-cols-md-2 gx-0 mb-0 mb-md-4 mt-0 mt-md-4">
-          <div className="col order-1 order-md-2">
-            <ScrollAnimation animateIn="fadeInLeft" animateOnce={true}>
-              <div className="col about-text-col">
+        <ScrollAnimation animateIn="fadeIn">
+          <div className="row row-cols-1 row-cols-md-2 gx-5 mb-0 mb-md-4 mt-0 mt-md-5">
+            <div className="col order-1 order-md-2">
+              <div className="about-text-col">
                 <div className="about-text-container">
-                  <h4 className="about-text-title">Selection</h4>
+                  <h4 className="about-text-title">SELECTION</h4>
                   <p className="about-text">
                     With OVER 200,000 square feet of LVP in stock carrying 15-year residential or lifetime commercial warranties the quality and selection speaks for itself.
                   </p>
                 </div>
               </div>
-            </ScrollAnimation>
-          </div>
-          <div className="col order-2 order-md-1">
-            <ScrollAnimation animateIn="fadeInRight" animateOnce={true}>
-              <div className="col about-img-col">
+            </div>
+            <div className="col order-2 order-md-1">
+              <div className="about-img-col">
                 <ParallaxBanner
                   className="parallax-about-right"
                   layers={[
                     {
                       image: aboutimg1,
-                      amount: 0.5
+                      amount: 0.0
                     }
                   ]}
-                  style={{
-                    height: '400px'
-                  }}
                 >
                 </ParallaxBanner> 
               </div>
-            </ScrollAnimation>
+            </div>
           </div>
-        </div>
+        </ScrollAnimation>
       </div>
     </div>
-    )
+  )
 }
 
 export default About;
