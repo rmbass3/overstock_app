d0e3d47a3c5f27fd50553c3309c8674a1994e600
Ryan <rmbass3@gmail.com>
2022-08-13 18:51:16 -0400

    Added form to PopModal

--- src/components/PopModal.js
+++ src/components/PopModal.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react"
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
 
 function PopModal(props) {
+
   const [show, setShow] = useState(false)
 
   useEffect(() => {
@@ -11,13 +13,8 @@ function PopModal(props) {
     }, 1000)
   }, [])
 
-  const handleClose = () => {
-    setShow(false)
-    //setDisableShow(true)
-  }
+  const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
-
-
 
   return (
     <div>
@@ -25,19 +22,34 @@ function PopModal(props) {
         show={show}
         onHide={handleClose}
         centered
-        size="lg"
       >
         <Modal.Header closeButton>
-          <Modal.Title>Stay Informed</Modal.Title>
+          <Modal.Title className="text-center">Stay Informed</Modal.Title>
+        </Modal.Header>
+        <Modal.Header>
+          <div className="">Be the first to know about our specials and new arrivals!</div>
         </Modal.Header>
         <Modal.Body>
-          Be the first to know about special deals and offers!
+          <Form>
+            <Form.Group className="mb-3" controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group className="mb-2" controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email address" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formPhone">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control type="phone" placeholder="Enter phone number" />
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Submit</Button>
+          <Button className="modal-submit">Submit</Button>
         </Modal.Footer>
       </Modal>
     </div>
