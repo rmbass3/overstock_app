63c0d300bed0b1e9d410536a4b42bd541efd9ea2
Ryan <rmbass3@gmail.com>
2022-08-14 21:58:05 -0400

    Added refs for form inputs

--- src/components/PopModal.js
+++ src/components/PopModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
@@ -6,6 +6,9 @@ import Form from 'react-bootstrap/Form'
 function PopModal(props) {
 
   const [show, setShow] = useState(false)
+  const nameInput = useRef(null)
+  const emailInput = useRef(null)
+  const phoneInput = useRef(null)
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,6 +18,15 @@ function PopModal(props) {
 
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    let name = nameInput?.current?.value
+    let email = emailInput?.current?.value
+    let phone = phoneInput?.current?.value
+    //handleClose()
+    //console.log(name, email, phone)
+  }
 
   return (
     <div>
@@ -29,28 +41,42 @@ function PopModal(props) {
         <Modal.Header className="modal-bottom-header">
           <div className="modal-bottom-title">Be the first to know about our specials and new arrivals!</div>
         </Modal.Header>
-        <Modal.Body>
-          <Form>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
             <Form.Group className="mb-3" controlId="formName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Full name" />
+              <Form.Control 
+                type="text"
+                placeholder="Full name"
+                ref={nameInput}
+              />
             </Form.Group>
             <Form.Group className="mb-2" controlId="formEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control 
+                type="email" 
+                placeholder="Email" 
+                ref={emailInput}
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formPhone">
               <Form.Label>Phone</Form.Label>
-              <Form.Control type="phone" placeholder="Phone number" />
+              <Form.Control 
+                type="phone" 
+                placeholder="Phone number" 
+                ref={phoneInput}
+              />
             </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="dark" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="light" className="modal-submit">Submit</Button>
-        </Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="dark" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="light" type="submit" className="modal-submit">
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </div>
   )
