aed875293e391aac4e14f021dd2c3a8ba95a4fcc
Ryan <rmbass3@gmail.com>
2022-12-18 21:32:32 -0500

    Update contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,7 +52,7 @@ function Contact(props){
       <ScrollAnimation className="contact-containter-animation" animateIn="fadeIn" duration={2}>
         <div className="contact-container">
           <div className="contact-map-container">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3230.6589168693636!2d-80.03837414863509!3d35.93084792375043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88530b81c8f99bed%3A0x2d169abe421fbfec!2sOverstock%20Discount%20Flooring!5e0!3m2!1sen!2sus!4v1637281756852!5m2!1sen!2sus" title="contact-map" className="contact-map"/>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3231.083860961134!2d-80.0380776!3d35.9204473!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88530b81c8f99bed%3A0x2d169abe421fbfec!2sOverstock%20Discount%20Flooring!5e0!3m2!1sen!2sus!4v1671396790324!5m2!1sen!2sus" title="contact-map" className="contact-map"/>
           </div>
           <div className="contact-info-container">
             <div className="contact-left">
@@ -64,10 +64,10 @@ function Contact(props){
                 </h4>
                 <h4 className="contact-info">
                   <i className="fas fa-map-marker-alt contact-icons-left"></i>
-                  <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/place/Overstock+Discount+Flooring/@35.9308479,-80.0383688,17z/data=!3m1!4b1!4m5!3m4!1s0x88530b81c8f99bed:0x2d169abe421fbfec!8m2!3d35.9308436!4d-80.0361801?hl=en">
-                    1205 Elgin Ave <br/>
-                    High Point, NC <br/>
-                    27262
+                  <a target="_blank" rel="noreferrer" href="https://goo.gl/maps/FhjPGcsgPBfLev4X9">
+                    908 Old Thomasville Rd <br/>
+                    High Point, NC
+                    27260
                   </a>
                 </h4>
               </div>
@@ -93,7 +93,7 @@ function Contact(props){
               <h3 className="contact-hours-title">HOURS</h3>
               <div className="hours-flex-container">
                 <h4 className="contact-hours">M-F: 8AM-5PM</h4>
-                <h4 className="contact-hours">Sat: 9AM-12PM</h4>
+                <h4 className="contact-hours">Sat: 8AM-12PM</h4>
                 <h4 className="contact-hours">Sun: Closed</h4>
               </div>
             </div>
